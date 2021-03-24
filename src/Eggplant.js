0fa561c92e7da91a89500ba446af2a7c7d173c51
nurimeansworld <nurimeansworld@gmail.com>
2021-03-24 17:25:46 +0900

    ADD-eggplant 추가

---
+++ src/Eggplant.js
@@ -0,0 +1,8 @@
+
+function Eggplant() {
+  return (
+    <h3>나이가 들면 가지가 맛있어 진대여..</h3>
+  );
+}
+
+export default Eggplant;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './App';
+import Eggplant from './Eggplant';
+
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Eggplant />
   </React.StrictMode>,
   document.getElementById('root')
 );
+// NOTE:: ReactDom은 only render 1 component
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
