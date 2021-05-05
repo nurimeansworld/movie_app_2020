e1a5edd466d96b0f841c58a71b35323c9f5923d5
nurimeansworld <nurimeansworld@gmail.com>
2021-05-05 14:57:10 +0900

    ADD-router(react-router-dom)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { HashRouter, Route } from "react-router-dom";   // import router
+
+import Home from "./routes/Home";
+import About from "./routes/About";
 
 function App() {
-  return <div>App</div>;
+  // return <div>App</div>;
+
+  return(
+    <HashRouter>
+
+      {/* exact가 없으면 /about 과 / 겹쳐 보이게 됨 */}
+      {/* <Route path="/" component={Home}></Route> */}
+      <Route path="/" exact={true} component={Home}></Route>
+      <Route path="/about" component={About}></Route>
+    </HashRouter>
+  );
 }
 
 export default App;
