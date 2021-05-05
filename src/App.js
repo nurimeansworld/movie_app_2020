9b97ed2c37e23c7cbef6376445523bcef1061ca8
nurimeansworld <nurimeansworld@gmail.com>
2021-05-05 15:23:28 +0900

    ADD-6.2 Navigation and Link

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { HashRouter, Route } from "react-router-dom";   // import router
 
+import Navigation from "./components/Navigation";
 import Home from "./routes/Home";
 import About from "./routes/About";
 
@@ -9,9 +10,10 @@ function App() {
 
   return(
     <HashRouter>
-
+      <Navigation />
       {/* exact가 없으면 /about 과 / 겹쳐 보이게 됨 */}
       {/* <Route path="/" component={Home}></Route> */}
+      
       <Route path="/" exact={true} component={Home}></Route>
       <Route path="/about" component={About}></Route>
     </HashRouter>
---
+++ src/components/Navigation.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+function Navigation() {
+  return (
+    <div>
+      {/* a 태그로 설정하면 페이지 리로드 */}
+      {/* <a herf="/">Home</a>
+      <a href="/about">About</a> */}
+
+      {/* Link로 설정 */}
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+    </div>
+  );
+}
+
+export default Navigation;
